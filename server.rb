f58a62566bc77a1cff918044dfee65b09e19f5e9
LindaKadz <achielinda44@gmail.com>
2018-07-04 12:53:23 +0300

    enabling deposting & withdrawal from browser

--- server.rb
+++ server.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require './account'
 
 get '/' do
   'Hello World!'
@@ -6,10 +7,6 @@ end
 
 get '/linda' do
   'Hello World!'
-end
-
-get '/:name' do
-  "Hello #{params[:name]}"
 end
 
 get '/' do
@@ -20,6 +17,27 @@ get '/name' do
   'matched name'
 end
 
-get '/:age' do
-  'matched age'
+get '/deposit/:amount' do
+  "You have deposited #{params[:amount]}"
+end
+
+get '/deposit' do
+  acc = Account.new
+  amount = params[:amount].to_i
+  acc.deposit(amount)
+  "Your new balance is #{acc.balance}"
+end
+
+get '/withdraw' do
+  erb :withdraw
+end
+
+post '/withdraw' do
+  acc = Account.new
+  amount = params[:amount].to_i
+  agent_number = params[:agent_number].to_i
+  pin = params[:pin].to_i
+  acc.withdraw(amount, agent_number, pin)
+
+  "You have withdrawn #{amount}, your balance is #{acc.balance}"
 end
